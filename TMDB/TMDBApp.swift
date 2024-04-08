7601bad3df720ca2754d73fa9dadbdeb1784421a
hauntarl <sameer.mungole@gmail.com>
2024-04-08 08:11:25 -0500

    Initial setup

---
+++ TMDB/TMDBApp.swift
@@ -0,0 +1,17 @@
+//
+//  TMDBApp.swift
+//  TMDB
+//
+//  Created by Sameer Mungole on 4/8/24.
+//
+
+import SwiftUI
+
+@main
+struct TMDBApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
